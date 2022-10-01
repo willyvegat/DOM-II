f9defe2fa4ac4adcf33942ba360ce26df63ed379
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-09-30 22:37:21 -0400

    first three event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// 1-load
+// window.onload = function (evt) {
+//     console.log(`event ${evt.type} fired! Ready to go!`);
+//     const heading = document.querySelector("h1");
+//     heading.textContent = "READY TO GO!";
+window.addEventListener("load", (event) => {
+    console.log(`event ${event.type} is fully Loaded and Ready`);
+    const heading = document.querySelector("h1");
+    heading.textContent = "Here We GOO!";
+
+    // 2-copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text;
+                //console.log(text);
+            })
+    })
+
+    // 3-click
+    document.body.addEventListener("click", (evt) => {
+        evt.target.classList.toggle("mirror");
+    })
+})
